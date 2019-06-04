deba40c363dde603f82769ee4e40d9876f18ee47
Jerrell Boone <jerrell.boone.13@gmail.com>
2019-06-04 08:49:42 -0400

    added social media links

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 var distance = $('nav').offset().top,
     $window = $(window);
 
+
 $window.scroll(function () {
     if ($window.scrollTop() >= distance - 1) {
         $('nav').addClass('nav-sticky')
+        $('.social-media-links').show()
+
     }
     else
     {
         $('nav').removeClass('nav-sticky')
-
+        $('.social-media-links').hide()
     }
+
 });
