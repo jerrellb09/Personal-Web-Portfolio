8b30a302e1ab7e91995ae57341aecd6392cdb018
Jerrell Boone <jboon001@fiu.edu>
2019-06-09 22:17:58 -0400

    minor update to elements

--- js/app.js
+++ js/app.js
@@ -8,10 +8,31 @@ $window.scroll(function () {
         $('.social-media-links').show()
 
     }
-    else
-    {
+    else {
         $('nav').removeClass('nav-sticky')
         $('.social-media-links').hide()
     }
-
 });
+
+
+var i = 0;
+var txt = "J E R R E L L"
+var txt2 = "Web Developer"
+var speed = 100;
+
+var typewriter = (function typeWriter() {
+    var name = document.getElementById("name");
+    name.classList.add("whoami-name");
+
+    if (i < txt.length) {
+        document.getElementById("name").innerHTML += txt.charAt(i);
+        // document.getElementById("developer").innerHTML += txt2.charAt(i);
+
+        i++;
+        setTimeout(typeWriter, speed);        
+    }    
+
+})();
+
+setTimeout(typewriter, 2000);
+
